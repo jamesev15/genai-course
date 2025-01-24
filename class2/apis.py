6810352ca8588e4c36f4cdf153a98f3a825fc25c
James Espichan <james.espichan@latam.com>
2025-01-23 21:56:21 -0500

    add request example

---
+++ class2/apis.py
@@ -0,0 +1,31 @@
+import requests
+import os
+
+tavily_url = "https://api.tavily.com/search"
+headers = {"Content-Type": "application/json"}
+
+
+def get_info(query):
+    data = {
+        "api_key": os.getenv("TAVILY_API_KEY"),
+        "query": query,
+    }
+
+    response = requests.post(
+        url=tavily_url,
+        headers=headers,
+        json=data
+    )
+
+    # print(response.status_code)
+    response = response.json()
+
+    resumen = ""
+
+    for result in response["results"]:
+        resumen = resumen + result["content"] + "\n"
+
+    return resumen
+
+query = input("introduce tu query: ")
+print(get_info(query))
